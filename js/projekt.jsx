77294527f053d8c0f6e7a42db08687374ac43dfd
Wojciech Nowacki <w.nowacky@gmail.com>
2018-02-21 20:59:55 +0100

    day1

--- js/projekt.jsx
+++ js/projekt.jsx
@@ -3,5 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../css/main.css';
 import '../scss/main.scss';
+import {Router,Route,Link,IndexLink,IndexRoute,hashHistory} from 'react-router';
 
-console.log("lll")
+
+
+
+//
+//
+//
+//
+// class Template extends React.Component{
+//     render() {
+//     }
+// }
+//
+//
+//
+// class App extends React.Component {
+//     render (){
+//         return <Router history={hashHistory}>
+//                 <Route path='/' component={Template}>
+//                     <IndexRoute component={Main}/>
+//                 </Route>
+//         </Router>
+//     }
+// }
+//
+//
+//
+// document.addEventListener('DOMContentLoaded', function(){
+//     ReactDOM.render(
+//         <App/>,
+//         document.getElementById('app')
+//     );
+// });
